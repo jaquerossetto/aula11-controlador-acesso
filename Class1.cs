3517d757a83d99261868d79fedce95583719361d
Jaqueline <jaquerossetto@hotmail.com>
2022-06-20 17:38:20 -0300

    Adicionar arquivos de projeto.

---
+++ Class1.cs
@@ -0,0 +1,28 @@
+﻿namespace aula11_controlador_acesso
+{
+    public class Nascimento
+    {
+    public string nome = "";
+    protected int idade;
+    public const int anoAtual = 2022;
+    protected int anoNascimento;
+
+    public int CalcularAnoNascimento()
+    {
+        anoNascimento = anoAtual - idade;
+        return anoNascimento;
+    }
+    public void ApresentarUsuario()
+    {
+        Console.WriteLine($"Olá {nome}, você nasceu em {anoNascimento}");
+    }
+
+    public void pegarInput()
+    {
+        Console.Write("Digite seu Nome: ");
+        nome = Console.ReadLine();
+        Console.WriteLine("Digite sua idade");
+        idade = int.Parse(Console.ReadLine());
+    }
+}
+    }
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace aula11_controlador_acesso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Nascimento nasc = new();
+            nasc.pegarInput();
+            nasc.CalcularAnoNascimento();
+            nasc.ApresentarUsuario();
+        }
+    }
+}
